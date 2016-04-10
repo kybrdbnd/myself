0e28b9ba3e9f737650abbf95f89cfab437e14af8
Pranav Puri <pranavpuri.p@hotmail.com>
2016-04-10 17:37:22 +0530

    deploying

--- myself/settings/base.py
+++ myself/settings/base.py
@@ -13,7 +13,7 @@ https://docs.djangoproject.com/en/1.8/ref/settings/
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 import os
 
-BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 
 
 # Quick-start development settings - unsuitable for production
@@ -113,7 +113,7 @@ EMAIL_HOST_USER = 'kybrdbnd@gmail.com'
 EMAIL_HOST_PASSWORD = 'keyboard007'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
-EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+# EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
 
 # message settings
 MESSAGE_STORAGE = 'django.contrib.messages.storage.cookie.CookieStorage'
---
+++ myself/settings/production.py
@@ -0,0 +1,29 @@
+import os
+from django.conf import settings
+import dj_database_url
+
+DEBUG = False
+TEMPLATE_DEBUG = False
+DATABASES = settings.DATABASES
+# STATICFILES_STORAGE = settings.STATICFILES_STORAGE
+# Parse database configuration from $DATABASE_URL
+
+
+DATABASES['default'] = dj_database_url.config()
+
+# Honor the 'X-Forwarded-Proto' header for request.is_secure()
+STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
+SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
+
+# Allow all host headers
+ALLOWED_HOSTS = ['*']
+
+# Static asset configuration
+
+# BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+# STATIC_ROOT = 'staticfiles'
+# STATIC_URL = '/static/'
+
+# STATICFILES_DIRS = (
+#     os.path.join(BASE_DIR, 'static'),
+# )
